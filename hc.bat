ddc290edd8f5e7ea1cd06c3c650bd5ed0488d000
Marcelle von Wendland <mvw@hivves.com>
2015-12-22 17:52:16 +0000

    init

---
+++ hc.bat
@@ -0,0 +1,1 @@
+java -cp "target\scala-2.11\antlr-test1_2.11-0.1.0.jar;lib\antlr-4.5.1-complete.jar;lib\sbt-antlr4-0.7.2.jar" com.example.CompilerApplication
--- project/sbt-antlr4.sbt
+++ project/sbt-antlr4.sbt
@@ -2,3 +2,4 @@
 
 addSbtPlugin("com.simplytyped" % "sbt-antlr4" % "0.7.2")
 
+
