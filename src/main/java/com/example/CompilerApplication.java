73bda351b9fc5cf09421f5e2f446e625191a8c0c
Marcelle von Wendland <mvw@hivves.com>
2015-12-22 18:07:51 +0000

    init

--- src/main/java/com/example/CompilerApplication.java
+++ src/main/java/com/example/CompilerApplication.java
@@ -6,16 +6,18 @@ import org.antlr.v4.runtime.tree.*;
 public class CompilerApplication {
     public static void main(String[] args) throws Exception {
 
-        String inputText = "{1,2,3,4}";
+        //String inputText = "{1,2,3,4}";
+        String inputFile = "test.me";
         if (args != null && args.length > 0) {
-            inputText = args[0];
+            inputFile = args[0];
         }
 
         // create a CharStream that reads from standard input
-        ANTLRInputStream input = new ANTLRInputStream(inputText);
+        //ANTLRInputStream input = new ANTLRInputStream(inputText);
 
         // create a lexer that feeds off of input CharStream
-        ArrayInitLexer lexer = new ArrayInitLexer(input);
+        //ArrayInitLexer lexer = new ArrayInitLexer(input);
+        ArrayInitLexer lexer = new ArrayInitLexer( new ANTLRFileStream(args[0]) );
 
         // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
